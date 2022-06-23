b94f050c3010c2af3881b5515b97d2f10b40b897
Nicholas Lona <101734110+nicklona17@users.noreply.github.com>
2022-06-23 13:45:51 -0400

    added portfolio carousel + contact modal

--- script.js
+++ script.js
@@ -10,3 +10,57 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     });
 });
+
+const carouselButtons = document.querySelectorAll("[data-carousel-button]");
+
+carouselButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        const slides = button.closest("[data-carousel]").querySelector("[data-slides]");
+
+        const activeSlide = slides.querySelector("[data-active]");
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex = slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+    });
+});
+
+const openModalButtons = document.querySelectorAll("[data-modal-target]");
+const closeModalButtons = document.querySelectorAll("[data-close-modal-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+overlay.addEventListener("click", () => {
+    const modals = document.querySelectorAll(".contact-modal.active");
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const modal = button.closest(".contact-modal");
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+}
